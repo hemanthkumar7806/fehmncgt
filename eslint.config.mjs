5989b9df0488870894d7cf14e97d9510ae7dd662
Lubin Raj <66677910+LubinRaj@users.noreply.github.com>
2025-11-06 19:41:11 +0530

    Update ESLint configuration: reorder extends, modify rules for TypeScript, and add new ignores for config files and dist directory.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,19 +10,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "warn"
-    },
     ignores: [
       "node_modules/**",
       ".next/**",
       "out/**",
       "build/**",
+      "dist/**",
+      "*.config.js",
+      "*.config.mjs",
       "next-env.d.ts",
     ],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
   },
 ];
 
